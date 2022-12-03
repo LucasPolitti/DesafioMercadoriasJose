5a36a79038c7f73afc5afc7985e98b85ddd0c9ec
Lucas Politti <lucaspolitti7@gmail.com>
2022-12-03 13:51:27 -0300

    Desafio José

---
+++ DesafioCodigoArrays.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class DesafioCodigoArrays {
+    public static void main(String[] args) {
+        
+    List<Integer> mercadoriasJose = Arrays.asList (2, 3, 5, 7, 13, 18, 34);
+
+    List<Integer> mercadoriasJoseUrgentes = mercadoriasJose.stream()
+    .filter(i -> (i % 2 == 0))
+    .collect(Collectors.toList());
+
+    System.out.println(mercadoriasJoseUrgentes);
+
+
+
+
+    }
+
+
+}
